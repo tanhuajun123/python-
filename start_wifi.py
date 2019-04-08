29aa01e71ffdcca3cb9946d4920b17aa28e4a0b6
jack.tan <32095378+tanhuajun123@users.noreply.github.com>
2019-04-08 18:03:28 +0800

    Add files via upload

--- start_wifi.py
+++ start_wifi.py
@@ -2,31 +2,58 @@
 #_*_coding:utf-8 _*_
 import os
 import time
-class APP():
+
+import os
+import time
+
+class App():
+
     def __init__(self,count):
         self.count = count
+    def device(self):
+        device = "adb devices "
+        src_outer = os.popen(device).read().strip()
+        if not src_outer:
+            print('cmd没有提示消息 ' + device)
+            sys.exit(0)
+        touch_info = 'List of devices attached'
+        lists = src_outer[src_outer.index(touch_info) + len(touch_info):]
 
-        #开启wifi的方法
-        def openwifi(self):
-            cmd = "adb shell svc wifi enable"
-            os.popen(cmd)
-            time.sleep(60)
+        if not lists:
+            print("没有设备连接电脑，请检查连接 ! ")
+            sys.exit(0)
 
-        #关闭wifi的方法
-        def closewifi(self):
-            cmd = "adb shell svc wifi disable"
-            time.sleep(5)
-        #控制wifi循环的方法
-        def controlwifi(self):
-            i = 1
-            while (self,count > 0 ):
-                print("第 %d 次执行开关WiFi 操作" % i)
-                self.closewifi()
-                self.closewifi()
-                i = i + 1
-                slef.count = self.count - 1
+    # 开启wifi的方法
+    def openWifi(self):
+        cmd = 'adb shell svc wifi enable'
+        os.popen(cmd)
+        time.sleep(10)
 
-if __name__ == "__main__":
-    #控制开关次数
-    app = APP()
-    app.controlwifi()
+    def scan(self):
+        cmd = "adb shell ping 127.0.0.1"
+        os.popen(cmd)
+        time.sleep(1)
+
+
+    # 关闭wifi的方法
+    def closeWifi(self):
+        cmd = 'adb shell svc wifi disable'
+        time.sleep(10)
+
+    #控制wifi循环的方法
+    def controlWifi(self):
+        i = 1
+        while (self.count >0):
+            print("第 %d 次执行开关Wi-Fi操作" % i)
+            self.device()
+            self.closeWifi()
+            self.scan()
+            self.openWifi()
+            i = i +1
+            self.count = self.count - 1
+
+
+if __name__ == '__main__':
+    #控制Wi-Fi开关执行100次
+    app = App(10)
+    app.controlWifi()
